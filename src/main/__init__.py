666556d5895b611b7ced474c41a0ab8dd74b82ee
ammy413 <qqlixiaodong@sina.com>
2016-09-09 21:32:02 +0800

    几种文本的显示
    几种文本的显示

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -22,9 +22,12 @@ tkinter.Button(root, text="设置按钮状态",width=21,state=DISABLED).pack()
 
 tkinter.Button(root, text = "输出文本", command = outputletter).pack()
 
-# tr_text = tkinter.Text(root)
-# tr_text.insert(1.0, '这个是可输入的文本框')
-# tr_text.pack()
+tr_text = tkinter.Text(root)
+tr_text.insert(1.0, '这个是可输入的文本框')
+tr_text.pack()
+
+tr_label = tkinter.Label(root,text='本周已工作')
+tr_label.pack()
 
 start.pack()
 
